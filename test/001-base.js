0a279a74d3bce70d470e763f90e5eb4239ff73f1
Emmanuel Raji <rojo.mosco@gmail.com>
2019-06-25 17:09:36 +0100

    ft(CI): update CI config - fix coveralls badge error - update travise config
    [Finishes: #Travis-CI]

--- test/001-base.js
+++ test/001-base.js
@@ -1,7 +1,5 @@
 import expect from 'expect';
 import request from 'supertest';
-import fs from 'fs';
-import path from 'path';
 import models from '../models';
 import app from '../app';
 
@@ -10,13 +8,8 @@ const user = { token: '' };
 
 describe('SETUP TEST', () => {
   before(async () => {
-    const sql = await fs.promises.readFile(
-      path.join(__dirname, '../migrations/tshirtshop.sql'),
-      'utf8'
-    );
-    await models.sequelize.query(sql);
+    await models.sequelize.sync();
   });
-
   it('should reply 404 error', async () => {
     const response = await request(app)
       .get('/fake-route')
